bd3a3eda5df1dee0775cb20a47f31bf8411908f8
azizkale <azizkale@hotmail.com>
2024-05-17 10:35:37 +0200

    initial commit

---
+++ src/main/java/com/kale/jwt_tutorial/JwtTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.kale.jwt_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kale/jwt_tutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kale.jwt_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
